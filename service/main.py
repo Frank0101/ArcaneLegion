4df67bb2ab130c14a42da17cd4de90513e8ec0ee
Francesco Cossu <francesco.cossu@gmail.com>
2026-04-23 12:03:42 +0100

    Small refactoring

--- service/main.py
+++ service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+
 from api.routes_health import router as health_router
 
-app = FastAPI()
+app = FastAPI(title="ArcaneLegion", version="0.1.0")
 
 app.include_router(health_router)
